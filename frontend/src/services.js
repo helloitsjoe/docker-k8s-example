3a745a0b8a8b4cfa0353eedb5ae76a62fd3bcd64
helloitsjoe <jboyle617@gmail.com>
2020-04-06 06:01:13 -0400

    docker-compose

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,8 +1,12 @@
+const KUBE_NODE_URL = 'http://192.168.64.2:31577';
+const DOCKER_COMPOSE_URL = 'http://localhost:8080';
+const DOCKER_LOCAL_URL = 'http://localhost:5000';
+
 const wait = () => new Promise(resolve => setTimeout(resolve, 200));
 
 export const fetchData = value => {
   return wait().then(() =>
-    fetch(`http://localhost:5000?sentence=${value}`)
+    fetch(`${KUBE_NODE_URL}?sentence=${value}`)
       .then(res => Promise.all([res.ok, res.json()]))
       .then(([ok, jsonOrErr]) => {
         if (!ok) throw new Error(jsonOrErr.message);
