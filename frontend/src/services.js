14bb4b575ba9a82baafc89fe9bdafb2d1bbcac43
helloitsjoe <jboyle617@gmail.com>
2020-05-13 06:09:34 -0400

    Ingress

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,12 +1,10 @@
-const KUBE_NODE_URL = 'http://192.168.64.2:31979';
-const DOCKER_COMPOSE_URL = 'http://localhost:8080';
-const DOCKER_LOCAL_URL = 'http://localhost:5000';
+const LOCAL_URL = 'http://localhost:8080';
 
 const wait = () => new Promise(resolve => setTimeout(resolve, 200));
 
 export const fetchData = value => {
   return wait().then(() =>
-    fetch(`${KUBE_NODE_URL}?sentence=${value}`)
+    fetch(`/sentiment?sentence=${value}`)
       .then(res => Promise.all([res.ok, res.json()]))
       .then(([ok, jsonOrErr]) => {
         if (!ok) throw new Error(jsonOrErr.message);
